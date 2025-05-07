cc5042aebf0f12ad3d7c081fd6a7baa429df26d3
HuascarDuran <avendur360@gmail.com>
2025-05-07 19:41:41 -0400

    Upload: Archivos de carpeta App
    Subo así los archivos porque si toco el git de mi proyecto en backend se arruinará y no quiero perjudicar a mi grupo ingeniero, mil disculpas. Esto es la carpeta App

---
+++ app/routes/user.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.session import SessionLocal
+from app.schemas.user import UserCreate, UserOut
+from app.models.user import User
+from app.core.security import get_password_hash
+from fastapi import status
+from app.schemas.user import UserLogin
+from app.core.security import create_access_token, authenticate_user
+from datetime import timedelta
+from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/register", response_model=UserOut)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email ya registrado")
+    hashed_password = get_password_hash(user.password)
+    new_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = authenticate_user(db, user.email, user.password)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales inválidas")
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    token = create_access_token(data={"sub": db_user.email}, expires_delta=access_token_expires)
+    
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.get("/", response_model=list[UserOut])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
